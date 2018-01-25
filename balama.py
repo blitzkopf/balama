c99520a4ef68457da8d1b6a4069f65fa935e1147
Yngvi Þór Sigurjónsson <blitzkopf@gmail.com>
2018-01-25 00:10:41 +0000

    adding coin pools query. adding graphite server cli parameter.

--- balama.py
+++ balama.py
@@ -10,12 +10,14 @@ parser.add_argument('--interval','--interval-seconds','-i', metavar='interval',
                    help='seconds to wait between data collections ')
 parser.add_argument('--repeats','-repeat-count','-r', metavar='repeats',
                    help='number of data collections before exiting ')
+parser.add_argument('--server','--graphite-server','-s', metavar='server',
+                   help='seconds to wait between data collections ',default='localhost')
 parser.add_argument('hosts', metavar='host', nargs='+',	
                    help='hostname to collect data from ')
 
 args = parser.parse_args()
 
-sender = graphite.Sender()
+sender = graphite.Sender(server=args.server)
 
 for host in args.hosts:
   cgminer = Cgminer(host=host)
--- data_collector.py
+++ data_collector.py
@@ -17,12 +17,13 @@ class Collector(threading.Thread):
 		self.sender = sender
 		self.interval_sec = interval_sec
 		self.repeats = repeats
-		self.version = self.miner.version()
-		self.tags = {'host':hostname,'type':id_cleanup(self.version['Type'])}
+		
 
 	def tick(self):
 		self.send_summary()
 		self.send_stats()
+		self.send_coin()
+		self.send_pools()
 		
 	def send_summary(self):
 		tm=int(time.time())
@@ -52,13 +53,39 @@ class Collector(threading.Thread):
 		#print(res)
 		#return res
 
+	def send_coin(self):
+		tm=int(time.time())
+		coin = self.miner.coin()
+		#print(summary)
+		self.sender.send({'Network_Difficulty':coin['Network Difficulty']},tm,self.tags,'coin.')
+
+	def send_pools(self):
+		tm=int(time.time())
+		pools = self.miner.pools()
+		i=0
+		for pool in pools:
+			res=self.numbers_only(pool)
+			self.sender.send(res,tm,self.tags,'pools.'+str(i))
+			i += 1
+		
+	def numbers_only(self,data):
+		res=dict()
+		for key,value in data.items():
+			try:
+				float(value)
+				res[id_cleanup(key)]=value
+			except ValueError:
+				pass
+		return res
 
 	def run(self):
+		self.version = self.miner.version()
+		self.tags = {'host':self.hostname,'type':id_cleanup(self.version['Type'])}
 		for i in range(self.repeats):
 			current_time = time.time()
 			sleeptime = (self.start_time+(self.interval_sec*(i)-current_time))
 			if(sleeptime>0):
-				print(sleeptime) 
+				#print(sleeptime) 
 				time.sleep(sleeptime)
 			self.tick()
 
